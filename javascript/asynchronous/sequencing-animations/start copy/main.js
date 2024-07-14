e973176ef8baabc8d0999d6452c55dd89a50354e
Caden Jamason <me@lutrine.dev>
2024-07-14 14:47:39 -0700

    Finished async animation problem

---
+++ javascript/asynchronous/sequencing-animations/start copy/main.js
@@ -0,0 +1,46 @@
+const aliceTumbling = [
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: "forwards",
+};
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+//! NOTE: `element.animate()` does *not* return a `Promise`: it returns an 
+//!       `Animation` object with a `finished` property that is a `Promise`.
+
+// Promise hell
+// alice1?.animate(aliceTumbling, aliceTiming).finished.then(() => {
+//   const aliceAnimation2 = alice2?.animate(aliceTumbling, aliceTiming).finished;
+//   aliceAnimation2?.then(() => {
+//     alice3?.animate(aliceTumbling, aliceTiming);
+//   });
+// });
+
+// Promise chain
+// alice1
+//   .animate(aliceTumbling, aliceTiming)
+//   .finished.then(() => alice2?.animate(aliceTumbling, aliceTiming).finished)
+//   .then(() => alice3?.animate(aliceTumbling, aliceTiming).finished)
+//   .catch((error) => console.error(`Error animating Alices: ${error}`));
+
+// Async and await
+async function animateAlices() {
+  try {
+    await alice1?.animate(aliceTumbling, aliceTiming).finished;
+    await alice2?.animate(aliceTumbling, aliceTiming).finished;
+    await alice3?.animate(aliceTumbling, aliceTiming).finished;
+  } catch (error) {
+    console.error(`Error animating Alices: ${error}`);
+  }
+}
+
+animateAlices();
+
